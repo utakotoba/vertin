8a46913d1b5e703a6f134a61ca050a04b8563d4c
Akiyama Kohane <echo.aimless@gmail.com>
2025-08-27 11:17:48 +0800

    feature(command): add helper function to define a root command

--- packages/vertin/src/helper.ts
+++ packages/vertin/src/helper.ts
@@ -29,3 +29,36 @@ export function defineCommand<T extends CommandOption>(
 
   return command
 }
+
+export function defineRootCommand<T extends CommandOption>(
+  option: T & {
+    readonly arguments?: {
+      [K in keyof T['arguments']]: _ArgumentParameterOption<NonNullable<T['arguments']>[K]>
+    }
+    readonly flags?: {
+      [K in keyof T['flags']]: _FlagParameterOption<NonNullable<T['flags']>[K]>
+    } & {
+      // disable metadata and subcommands for root command
+      readonly name: never
+      readonly description: never
+      readonly version: never
+      readonly subCommands: never
+    }
+  },
+  handler: CommandHandler<T>,
+): Command {
+  const command: Command = {
+    metadata: {
+      name: '__root__',
+      description: '__root_description__',
+    },
+    exec: {
+      name: '__root__',
+      arguments: option.arguments ?? {},
+      flags: option.flags ?? {},
+    },
+    handler,
+  }
+
+  return command
+}
