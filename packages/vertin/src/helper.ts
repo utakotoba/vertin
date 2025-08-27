9a59887dfdb5bdd1145f809111ba09964b103ea2
Akiyama Kohane <echo.aimless@gmail.com>
2025-08-27 11:09:13 +0800

    feat(command): add helper function to creare command

---
+++ packages/vertin/src/helper.ts
@@ -0,0 +1,31 @@
+import type { Command, CommandHandler, CommandOption } from './types/command'
+import type { _ArgumentParameterOption, _FlagParameterOption } from '@/parser'
+
+export function defineCommand<T extends CommandOption>(
+  option: T & {
+    readonly arguments?: {
+      [K in keyof T['arguments']]: _ArgumentParameterOption<NonNullable<T['arguments']>[K]>
+    }
+    readonly flags?: {
+      [K in keyof T['flags']]: _FlagParameterOption<NonNullable<T['flags']>[K]>
+    }
+  },
+  handler: CommandHandler<T>,
+): Command {
+  const command: Command = {
+    metadata: {
+      name: option.name,
+      description: option.description,
+      version: option.version,
+    },
+    exec: {
+      name: option.execName ?? option.name,
+      arguments: option.arguments ?? {},
+      flags: option.flags ?? {},
+    },
+    handler,
+    subCommands: option.subCommands,
+  }
+
+  return command
+}
